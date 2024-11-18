8411e7de59423cad58ca2087eb4b8c917299eadd
SantiagoBreton <128427309+SantiagoBreton@users.noreply.github.com>
2024-11-18 00:36:43 -0300

    Enhance event subscription logic with participant checks and error handling

--- src/controllers/subscribeToAnEvent.ts
+++ src/controllers/subscribeToAnEvent.ts
@@ -4,6 +4,21 @@ import prisma from '../prisma';
 export const subscribeToEvent = async (req: Request, res: Response) => {
   const { userId, eventId } = req.body;
   try {
+    const event = await prisma.event.findUnique({
+      where: {
+        id: eventId,
+      },
+    });
+    
+    if (!event) {
+      res.status(404).json({ error: 'Event not found.' });
+      return;
+    }
+
+    if (event.currentParticipants === event.maxParticipants) {
+      res.status(400).json({ error: 'Que desgracia Amigo, este evento ya esta lleno' });
+      return;
+    }
     const eventUser = await prisma.eventUser.create({
       data: {
         userId,
@@ -11,9 +26,20 @@ export const subscribeToEvent = async (req: Request, res: Response) => {
       },
     });
 
+    const updateEventParticipants = await prisma.event.update({
+      where: {
+        id: eventId,
+      },
+      data: {
+        currentParticipants: {
+          increment: 1,
+        },
+      },
+    });
+
     res.status(201).json(eventUser);
   } catch (error) {
-    res.status(500).json({ error: 'An error occurred while subscribing to the event.' });
+    res.status(500).json({ error: 'No has podido suscribirte al evento, intentalo ams tarde.' });
   }
 };
 
