c6b1fc3d9634c7ff8a44690059805e1eb947f5b7
MatiasdeBuren <64704995+MatiasdeBuren@users.noreply.github.com>
2025-01-23 16:32:17 -0300

    Delete previous review when adding a new one.

--- src/controllers/createNewRating.ts
+++ src/controllers/createNewRating.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from "express";
 import prisma from '../prisma';
 
-
 export const createNewUserRating = async (req: Request, res: Response) => {
     const { ratingUserId, userId, comment, rating } = req.body;
-    console.log(rating, userId);
 
+    // Check if the user has already rated this user
+    const existingRating = await prisma.userRatingByUser.findFirst({
+        where: {
+            ratingUserId,
+            userId,
+        },
+    });
+
+    // If a rating exists, delete the previous rating
+    if (existingRating) {
+        await prisma.userRatingByUser.delete({
+            where: { id: existingRating.id },
+        });
+    }
+
+    // Create the new rating
     const newUserRating = await prisma.userRatingByUser.create({
         data: {
             comment,
@@ -15,18 +29,17 @@ export const createNewUserRating = async (req: Request, res: Response) => {
         },
     });
 
-    // Calcular el promedio de todos los ratings del usuario
+    // Calculate the average rating of the user
     const averageRating = await prisma.userRatingByUser.aggregate({
         where: { userId },
         _avg: { rating: true },
     });
 
-    // Actualizar el campo `rating` del usuario
+    // Update the user's rating field
     await prisma.user.update({
         where: { id: userId },
-        data: { rating: averageRating._avg.rating || 0 }, // Por defecto 0 si no hay ratings
+        data: { rating: averageRating._avg.rating || 0 }, // Default to 0 if no ratings
     });
 
-
     res.json(newUserRating);
-}
+};
