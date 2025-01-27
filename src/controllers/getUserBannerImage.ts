5d6c38e9a472168bd56f2b0ff83a5cddfb09211b
SantiagoBreton <128427309+SantiagoBreton@users.noreply.github.com>
2025-01-27 14:09:27 -0300

    Improve error handling and formatting in getUserBannerImage controller

--- src/controllers/getUserBannerImage.ts
+++ src/controllers/getUserBannerImage.ts
@@ -4,8 +4,9 @@ import prisma from '../prisma';
 
 export const getUserBannerImage = async (req: Request, res: Response) => {
     const { userId } = req.params;
-
+    
     try {
+
         const userProfileImage = await prisma.userBannerImage.findMany({
             where: { userId: Number(userId) },
         });
@@ -16,8 +17,10 @@ export const getUserBannerImage = async (req: Request, res: Response) => {
 
         const imagePath = userProfileImage[0].imageUrl;
         const imageUrl = `http://${req.headers.host}/uploads/${path.basename(imagePath)}`;
+        //console.log('User banner image:', imageUrl);
         res.status(200).json({ imageUrl }); // Return the image URL
     } catch (error) {
+        
         console.error('Error fetching user profile image:', error);
         res.status(500).json({ error: 'Failed to get user profile image, please try again later.' });
     }
